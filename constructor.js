b9536a762ea4603f90564d77c30c8cabb17e8469
roshanba <roshantravis@gmail.com>
2019-09-11 11:42:05 +0300

    File stucture change as lecture"

---
+++ constructor.js
@@ -0,0 +1,11 @@
+// constructor function 
+
+function elf (name, weapon){
+    this.name=name;
+    this.weapon=weapon;
+    }
+
+const roshan= new elf('Roshan','knife');
+
+console.log(roshan.name);
+
